cdfdbdf2c8d73aba5c9181f3bc10df7008c7fc94
manar <mnaawaham2001ye@gmail.com>
2025-04-25 18:31:48 +0300

    added the code and requests of home page

--- blog_site/blog_app/views.py
+++ blog_site/blog_app/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect, render
+from .models import Post
+from .form import PostForm
 
-# Create your views here.
+def home(request):
+  post= Post.objects.all()
+  context={}
+  context["all_posts"]=post
+
+  return render(request,'home.html',context)
+
