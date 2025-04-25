b267284184284fc6683110450985485d0dc48368
manar <mnaawaham2001ye@gmail.com>
2025-04-25 19:01:21 +0300

    added create,update,delete pages to project

--- blog_site/blog_app/urls.py
+++ blog_site/blog_app/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 urlpatterns = [
     path('',views.home, name='home'),
+    path('<int:pk>/', views.details.as_view(), name='post_details'),
+    path('create_post/', views.Post_create.as_view(), name="post_create" ),
+    path('<int:pk>/update/', views.post_update.as_view(), name="post_update" ),
+    path('<int:pk>/delete/', views.post_delete.as_view(), name="post_delete" ),
 
 ]
--- blog_site/blog_app/views.py
+++ blog_site/blog_app/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import get_object_or_404, redirect, render
 from .models import Post
-from .forms import PostForm
+from .form import PostForm
+from django.views.generic.edit import CreateView,UpdateView,DeleteView
+from django.views.generic.detail import DetailView
+from django.urls import reverse_lazy
+# Create your views here.
 
 def home(request):
   post= Post.objects.all()
@@ -9,3 +13,35 @@ def home(request):
 
   return render(request,'home.html',context)
 
+
+class details(DetailView):
+  model=Post
+  template_name="detailsPage.html"
+  
+  
+class Post_create(CreateView):
+  
+  model=Post
+  
+  form_class=PostForm
+  
+  template_name="Post_create.html"
+  success_url=reverse_lazy("home")
+  
+  
+  
+ 
+
+class post_update(UpdateView):
+  model=Post
+  form_class=PostForm
+  template_name="post_update.html"
+  
+  
+  success_url=reverse_lazy("home")
+  
+  
+class post_delete(DeleteView):
+  model=Post
+  template_name="post_delete.html"
+  success_url=reverse_lazy("home")
